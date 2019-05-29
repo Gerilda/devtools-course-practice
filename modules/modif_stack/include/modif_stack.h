9c7023baa0fe18fb046295c2d001b0f5d53719f8
Anastasia <anagerilda@mail.ru>
2019-05-29 04:49:52 +0300

    Fixed syntax in #ifndef 3*_ lab3

--- modules/modif_stack/include/modif_stack.h
+++ modules/modif_stack/include/modif_stack.h
@@ -1,7 +1,7 @@
 // Copyright 2019 Karasev Anton
 
-#ifndef MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_H__
-#define MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_H__
+#ifndef MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_H___
+#define MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_H___
 
 #include <iostream>
 #include <vector>
@@ -27,4 +27,4 @@ class ModifStack {
     std::vector<std::pair<int, int>> ShowReverseStack();
 };
 
-#endif  // MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_H__
+#endif  // MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_H___
