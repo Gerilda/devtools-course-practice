2c2e7bce85f1616be7ce94db8fbde5e751b53c54
Anastasia <anagerilda@mail.ru>
2019-05-29 05:16:31 +0300

    Fixed syntax in #ifndef AAAAAAAAAAAA lab3

--- modules/modif_stack/include/modif_stack_app.h
+++ modules/modif_stack/include/modif_stack_app.h
@@ -1,7 +1,7 @@
 // Copyright 2019 Trubina Anastasia
 
-#ifndef MODULES_MODIF_STACK_APP_INCLUDE_MODIF_STACK_APP_H_
-#define MODULES_MODIF_STACK_APP_INCLUDE_MODIF_STACK_APP_H_
+#ifndef MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_APP_H_
+#define MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_APP_H_
 
 #include <sstream>
 #include <string>
@@ -19,4 +19,4 @@ class ModifStackApp {
     std::string operator()(int argc, const char** argv);
 };
 
-#endif  // MODULES_MODIF_STACK_APP_INCLUDE_MODIF_STACK_APP_H_
+#endif  // MODULES_MODIF_STACK_INCLUDE_MODIF_STACK_APP_H_
